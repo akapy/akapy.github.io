e644eef72df7c0d700f9bec51affb8ec8d2859b3
akapy <hge02705@nifty.com>
2021-04-09 19:09:09 +0900

    4/9奥様は取り扱い注意

--- rchalow/cl.js
+++ rchalow/cl.js
@@ -1,14 +1,14 @@
 window.onload = function() {var latest_items = '';
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-11.html">2021-04-11</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-10.html">2021-04-10</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-09.html">2021-04-09</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2021-04-09-1.html">奥様は取り扱い注意</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-08.html">2021-04-08</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-07.html">2021-04-07</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-06.html">2021-04-06</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-05.html">2021-04-05</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-04.html">2021-04-04</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-03.html">2021-04-03</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-02.html">2021-04-02</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-04-01.html">2021-04-01</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-03-31.html">2021-03-31</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-03-30.html">2021-03-30</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-03-29.html">2021-03-29</a><br>');
-latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2021-03-29-5.html">今日の天気と万歩計と自分への誕生日プレゼント</a><br>');
 document.getElementById('latest_items').insertAdjacentHTML('afterbegin', latest_items);
 }
