45f3f5aa0adbff02894ddee37e6140c7c2436e04
akapy <hge02705@nifty.com>
2021-07-03 14:27:42 +0900

    7/3リオーネ

--- rchalow/cl.js
+++ rchalow/cl.js
@@ -1,6 +1,10 @@
 window.onload = function() {var latest_items = '';
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-07-04.html">2021-07-04</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-07-03.html">2021-07-03</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-07-02.html">2021-07-02</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2021-07-02-1.html">ファブル２</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-07-01.html">2021-07-01</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2021-07-01-1.html">るろうに剣心_最終章 The Beginning／劇場版シグナル</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-06-30.html">2021-06-30</a><br>');
 latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2021-06-30-1.html">コナン緋色の弾丸／ワクチン接種券</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-06-29.html">2021-06-29</a><br>');
@@ -9,7 +13,5 @@ latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/202
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-06-26.html">2021-06-26</a><br>');
 latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2021-06-26-5.html">今日も草刈りと収穫／今日の天気と万歩計</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-06-25.html">2021-06-25</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-06-24.html">2021-06-24</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-06-23.html">2021-06-23</a><br>');
 document.getElementById('latest_items').insertAdjacentHTML('afterbegin', latest_items);
 }
