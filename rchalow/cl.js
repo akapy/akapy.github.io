f4d4148fd1bacff1de82ba7a430ef87281482c2b
akapy <hge02705@nifty.com>
2021-10-31 15:01:25 +0900

    10/30imo

--- rchalow/cl.js
+++ rchalow/cl.js
@@ -1,13 +1,14 @@
 window.onload = function() {var latest_items = '';
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-11-01.html">2021-11-01</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-31.html">2021-10-31</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-30.html">2021-10-30</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2021-10-30-5.html">さつまいもを掘る／今日の天気と万歩計</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-29.html">2021-10-29</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-28.html">2021-10-28</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-27.html">2021-10-27</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-26.html">2021-10-26</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-25.html">2021-10-25</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-24.html">2021-10-24</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-23.html">2021-10-23</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-22.html">2021-10-22</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-21.html">2021-10-21</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-20.html">2021-10-20</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2021-10-19.html">2021-10-19</a><br>');
 document.getElementById('latest_items').insertAdjacentHTML('afterbegin', latest_items);
 }
