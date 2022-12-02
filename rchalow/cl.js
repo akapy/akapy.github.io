b72ee13b71d7b2afe88b8322756d42265daeb152
akapy <hge02705@nifty.com>
2022-12-03 08:38:59 +0900

    ワンピース12/3

--- rchalow/cl.js
+++ rchalow/cl.js
@@ -1,4 +1,5 @@
 window.onload = function() {var latest_items = '';
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2022-12-04.html">2022-12-04</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2022-12-03.html">2022-12-03</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2022-12-02.html">2022-12-02</a><br>');
 latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2022-12-02-1.html">ワンピースRED</a><br>');
@@ -9,6 +10,5 @@ latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/202
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2022-11-27.html">2022-11-27</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2022-11-26.html">2022-11-26</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2022-11-25.html">2022-11-25</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2022-11-24.html">2022-11-24</a><br>');
 document.getElementById('latest_items').insertAdjacentHTML('afterbegin', latest_items);
 }
