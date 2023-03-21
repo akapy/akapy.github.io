48166e1cfb08626b70626a8230cdba7279935b54
akapy <hge02705@nifty.com>
2023-03-21 10:13:27 +0900

    むろね庵そば

--- rchalow/cl.js
+++ rchalow/cl.js
@@ -1,7 +1,10 @@
 window.onload = function() {var latest_items = '';
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-22.html">2023-03-22</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-21.html">2023-03-21</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-20.html">2023-03-20</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2023-03-20-5.html">今日の天気と万歩計</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-19.html">2023-03-19</a><br>');
-latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2023-03-19-5.html">今日の天気と万歩計</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2023-03-19-5.html">春分の墓参り＋今日の天気と万歩計</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-18.html">2023-03-18</a><br>');
 latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2023-03-18-1.html">新年ホヤほやクーポン3000円を無駄にした。。</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-17.html">2023-03-17</a><br>');
@@ -9,7 +12,5 @@ latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/202
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-15.html">2023-03-15</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-14.html">2023-03-14</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-13.html">2023-03-13</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-12.html">2023-03-12</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-03-11.html">2023-03-11</a><br>');
 document.getElementById('latest_items').insertAdjacentHTML('afterbegin', latest_items);
 }
