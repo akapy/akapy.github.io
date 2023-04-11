4a67b5983c04e0472e6464183fdbe5ee31ccbe54
akapy <hge02705@nifty.com>
2023-04-11 09:37:36 +0900

    桜さしん2

--- rchalow/cl.js
+++ rchalow/cl.js
@@ -6,6 +6,7 @@ latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-04-09.html">2023-04-09</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-04-08.html">2023-04-08</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-04-07.html">2023-04-07</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2023-04-07-5.html">今日の天気と万歩計</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-04-06.html">2023-04-06</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2023-04-05.html">2023-04-05</a><br>');
 latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2023-04-05-5.html">今日の天気と万歩計</a><br>');
