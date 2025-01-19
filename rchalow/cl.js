c613fac50639934aeff97864bdd3cdde315ba4ea
akapy <hge02705@nifty.com>
2025-01-19 09:48:16 +0900

    確定申告

--- rchalow/cl.js
+++ rchalow/cl.js
@@ -1,5 +1,9 @@
 window.onload = function() {var latest_items = '';
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-16.html">2025-01-16</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-20.html">2025-01-20</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-19.html">2025-01-19</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-18.html">2025-01-18</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2025-01-18-1.html">debianとNFCスマホで確定申告</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-17.html">2025-01-17</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-16.html">2025-01-16</a><br>');
 latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2025-01-16-1.html">グランメゾンパリ</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-15.html">2025-01-15</a><br>');
@@ -7,8 +11,5 @@ latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/202
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-13.html">2025-01-13</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-12.html">2025-01-12</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-11.html">2025-01-11</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-10.html">2025-01-10</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-09.html">2025-01-09</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-01-08.html">2025-01-08</a><br>');
 document.getElementById('latest_items').insertAdjacentHTML('afterbegin', latest_items);
 }
