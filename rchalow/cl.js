679513b78a6d302c62d1909649bc623d564d4ced
akapy <hge02705@nifty.com>
2025-11-22 10:43:51 +0900

    爆弾2

--- rchalow/cl.js
+++ rchalow/cl.js
@@ -3,7 +3,7 @@ latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/202
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-11-23.html">2025-11-23</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-11-22.html">2025-11-22</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-11-21.html">2025-11-21</a><br>');
-latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2025-11-21-1.html">雪風</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2025-11-21-1.html">爆弾</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-11-20.html">2025-11-20</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-11-19.html">2025-11-19</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2025-11-18.html">2025-11-18</a><br>');
