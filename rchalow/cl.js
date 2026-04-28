f0fcbbad087f0d7d0a1285300073266a078bc739
akapy <hge02705@nifty.com>
2026-04-29 06:50:24 +0900

    なぜラブレター

--- rchalow/cl.js
+++ rchalow/cl.js
@@ -1,5 +1,9 @@
 window.onload = function() {var latest_items = '';
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-05-01.html">2026-05-01</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-30.html">2026-04-30</a><br>');
+latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-29.html">2026-04-29</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-28.html">2026-04-28</a><br>');
+latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2026-04-28-1.html">人はなぜラブレターを書くのか</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-27.html">2026-04-27</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-26.html">2026-04-26</a><br>');
 latest_items = latest_items.concat('- <a href="https://akapy.github.io/rchalow/2026-04-26-1.html">熊山で植樹会：彼峰の会：</a><br>');
@@ -7,8 +11,5 @@ latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/202
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-24.html">2026-04-24</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-23.html">2026-04-23</a><br>');
 latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-22.html">2026-04-22</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-21.html">2026-04-21</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-20.html">2026-04-20</a><br>');
-latest_items = latest_items.concat('<a href="https://akapy.github.io/rchalow/2026-04-19.html">2026-04-19</a><br>');
 document.getElementById('latest_items').insertAdjacentHTML('afterbegin', latest_items);
 }
